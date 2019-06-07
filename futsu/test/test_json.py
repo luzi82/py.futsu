94299ab91e0ee4e01bd6226bc68f028569b2dfda
Luzi82 <luzi82@gmail.com>
2019-06-07 10:21:01 +0800

    update futsu.json func name

--- futsu/test/test_json.py
+++ futsu/test/test_json.py
@@ -3,21 +3,21 @@ import futsu.json as fjson
 import tempfile
 import os
 
-class TestIo(TestCase):
+class TestJson(TestCase):
 
-    def test_json_read(self):
-        data = fjson.json_read(os.path.join('futsu','test','test_json_0.json'))
+    def test_file_input(self):
+        data = fjson.file_input(os.path.join('futsu','test','test_json_0.json'))
         self.assertEqual(data,{'qwer':'asdf'})
 
-    def test_json_write(self):
+    def test_file_output(self):
         with tempfile.TemporaryDirectory() as tempdir:
             tmp_filename = os.path.join(tempdir,'ALSFAWFHMY')
 
             self.assertFalse(os.path.isfile(tmp_filename))
 
             data={'qwer':'asdf'}
-            fjson.json_write(tmp_filename, data)
+            fjson.file_output(tmp_filename, data)
             self.assertTrue(os.path.isfile(tmp_filename))
             
-            data = fjson.json_read(tmp_filename)
+            data = fjson.file_input(tmp_filename)
             self.assertEqual(data,{'qwer':'asdf'})
