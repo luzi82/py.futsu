c74c76073f7b0e87162bb855a6c093d44f00fe6c
luzi82 <luzi82@gmail.com>
2019-10-16 22:13:28 +0800

    add s3 support

--- futsu/test/test_storage.py
+++ futsu/test/test_storage.py
@@ -27,3 +27,15 @@ class TestStorge(TestCase):
             storage.path_to_local(tmp_filename,tmp_gs_blob)
             
             self.assertFalse(fs.diff(tmp_filename,src_file))
+
+    def test_s3(self):
+        with tempfile.TemporaryDirectory() as tempdir:
+            tmp_filename = os.path.join(tempdir,'TMWGHOKDRE')
+            src_file = os.path.join('futsu','test','test_storage_0.txt')
+            timestamp = int(time.time())
+            tmp_path = 's3://futsu-test/test-KWPIYZVIYK-{0}'.format(timestamp)
+            
+            storage.local_to_path(tmp_path,src_file)
+            storage.path_to_local(tmp_filename,tmp_path)
+            
+            self.assertFalse(fs.diff(tmp_filename,src_file))
