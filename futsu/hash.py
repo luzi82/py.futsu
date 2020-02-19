807ea9dc54ee2e515ad71276bbb9d60b6fe20e39
luzi82 <luzi82@gmail.com>
2020-02-19 12:36:32 +0800

    sha256

--- futsu/hash.py
+++ futsu/hash.py
@@ -4,3 +4,8 @@ def md5_str(s):
     m = hashlib.md5()
     m.update(s.encode('utf8'))
     return m.hexdigest()
+
+def sha256_str(s):
+    m = hashlib.sha256()
+    m.update(s.encode('utf8'))
+    return m.hexdigest()
