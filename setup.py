8ccbf253f806136456b54a125a580a5d57aabc6e
luzi82 <luzi82@gmail.com>
2020-11-03 15:19:48 +0000

    r0.0.30

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="futsu",
-    version="0.0.29",
+    version="0.0.30",
     author="Luzi Leung",
     author_email="luzi82@gmail.com",
     description="Very reusable python code",
